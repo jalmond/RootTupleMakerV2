3daefbf3e7cf80d0da9a380f3b78b26ab6ef701d
John <jalmond@cern.ch>
2016-06-22 11:33:16 +0200

    update for no chs

--- python/RootTupleMakerV2_PFJets_cfi.py
+++ python/RootTupleMakerV2_PFJets_cfi.py
@@ -1,16 +1,16 @@
 import FWCore.ParameterSet.Config as cms
 
 rootTuplePFJets = cms.EDProducer("RootTupleMakerV2_PFJets",
-    InputTag = cms.InputTag('selectedPatJetsAK5PFchs'),
+    InputTag = cms.InputTag('selectedPatJetsAK5PF'),
     # InputTagL1Offset    = cms.InputTag('selectedPatJetsAK5PFL1Offset'),
-    InputTagSmearedUp   = cms.InputTag('smearedPatJetsAK5PFchsResUp'),                                 
-    InputTagSmearedDown = cms.InputTag('smearedPatJetsAK5PFchsResDown'),                                 
-    InputTagScaledUp    = cms.InputTag('shiftedPatJetsAK5PFchsEnUpForCorrMEt'),                                 
-    InputTagScaledDown  = cms.InputTag('shiftedPatJetsAK5PFchsEnDownForCorrMEt'),                                 
+    InputTagSmearedUp   = cms.InputTag('smearedPatJetsAK5PFResUp'),                                 
+    InputTagSmearedDown = cms.InputTag('smearedPatJetsAK5PFResDown'),                                 
+    InputTagScaledUp    = cms.InputTag('shiftedPatJetsAK5PFEnUpForCorrMEt'),                                 
+    InputTagScaledDown  = cms.InputTag('shiftedPatJetsAK5PFEnDownForCorrMEt'),                                 
     Prefix = cms.string('PFJet'),
     Suffix = cms.string(''),
     MaxSize = cms.uint32(30),
-    JECUncertainty = cms.string('AK5PFchs'),
+    JECUncertainty = cms.string('AK5PF'),
     ReadJECuncertainty = cms.bool(True),
     VertexInputTag = cms.InputTag('offlinePrimaryVertices')
 )
